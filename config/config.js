bfc4b7d6e5eb916c7d40a30e0667b93a625dafef
ubaid naeem <ubaid29170@gmail.com>
2025-05-09 01:45:59 +0500

    first commit

--- config/config.js
+++ config/config.js
@@ -1,25 +1,32 @@
 require('dotenv').config()
-module.exports ={
-  "development": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME,
-    "host":  process.env.DB_HOST,
-    "port" : process.env.DB_PORT,
-    "dialect": "postgres"
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false // Accept self-signed certificates (RDS default)
+      }
+    }
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-      "dialect": "postgres"
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "postgres"
   },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
+  production: {
+    username: "root",
+    password: null,
+    database: "database_production",
+    host: "127.0.0.1",
+    dialect: "postgres"
   }
 }
